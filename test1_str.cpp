ec730618db31278c35119c489213779687e9918e
jackwu <1683358846@qq.com>
2018-01-03 11:16:24 +0800

    test string

---
+++ test1_str.cpp
@@ -0,0 +1,59 @@
+//test string 
+//author :wuyujie
+#include <iostream>
+
+#include<string>
+
+using namespace std;
+
+
+
+
+
+int main(int argc,char **argv){
+
+
+
+     string str("wuyujie");
+     cout<<str<<endl;
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
